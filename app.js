f2ae7734c2531809cc69d61a0b5ada9f24886f61
Ubuntu <rex@python.p4emldm1axcu1iccvkk4q5ovpc.px.internal.cloudapp.net>
2021-04-07 08:17:46 +0000

    Numbers, Strings and Boolean

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Your code goes here...!');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,2 +1,16 @@
-console.log('Your code goes here...!')
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    let result = n1 + n2;
+    if(showResult){
+        console.log(phrase + result);
+    }else{
+        return result;
+    }
+}
 
+const number1 = 5;
+const number2 = 2.8;
+
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+const result = add(number1, number2, printResult,resultPhrase)
